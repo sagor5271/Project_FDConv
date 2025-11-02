4b27c7b19968febf371245f966bd509bc6ebb8c0
MD SAGOR HOSSAIN <sagor.bme.iu@gmail.com>
2025-11-02 21:33:41 +0600

    Comment out import of CONV_LAYERS from mmcv

--- FDConv.py
+++ FDConv.py
@@ -6,7 +6,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 from numpy.linalg import matrix_rank
 from torch.utils.checkpoint import checkpoint
-from mmcv.cnn import CONV_LAYERS
+#from mmcv.cnn import CONV_LAYERS
 from torch import Tensor
 import torch.nn.functional as F
 import math
